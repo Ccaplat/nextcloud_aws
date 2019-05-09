5d063a5b7cf1cff15ec47bd3561bfcdd488670b7
Ccaplat <ccaplat@tutanota.com>
2019-05-09 10:40:59 -0400

    second version

--- key.tf
+++ key.tf
@@ -1,3 +1,6 @@
+# KEY PAIR to use in order to connect to our instances
+# Key needs to be created before using terraform ,ssh-key -f command
+
 resource "aws_key_pair" "nextkey"{
   key_name = "nextkey"
   public_key = "${file("${var.public_key}")}"
